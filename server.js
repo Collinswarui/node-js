1a909c157b86b0924b8adf27a141b476bc799143
collins <collinswanjiru47@gmail.com>
2023-05-24 09:55:53 +0300

    Query String

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res)=> {
-    res.write("Hello you");
-    res.end();
+    res.writeHead(200, {'Content-Type':'text/html'});
+    const quer = url.parse(req.url, true).query;
+    const txt = quer.year + " " + quer.month;
+    res.end(txt);
 });
 
-server.listen(3000, () =>{
+server.listen(8080, () =>{
     console.log('The server is up and running')
 });
